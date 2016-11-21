bfe831337c6eb3dbce1dd4d732d21176cebd9f3e
mikejeffers <mike.s.jeffers@gmail.com>
2016-11-20 22:31:59 -0500

    quick test for grabbing market pricing; for #2

--- test/src/net/sf/freecol/common/model/MarketTest.java
+++ test/src/net/sf/freecol/common/model/MarketTest.java
@@ -19,6 +19,9 @@
 
 package net.sf.freecol.common.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import net.sf.freecol.common.model.Specification;
 import net.sf.freecol.util.test.FreeColTestCase;
 
@@ -38,12 +41,47 @@ public class MarketTest extends FreeColTestCase {
         Market dm = p.getMarket();
 
         Specification s = spec();
+        
 
         for (GoodsType good : s.getStorableGoodsTypeList()) {
             assertEquals(good.toString(), good.getInitialBuyPrice(), dm.getCostToBuy(good));
             assertEquals(good.toString(), good.getInitialSellPrice(), dm.getPaidForSale(good));
         }
     }
+    
+    
+    public void testEuropeMarketPricing(){
+    	 Game g = getStandardGame();
+
+         Player p = g.getPlayerByNationId("model.nation.dutch");
+
+         Specification s = spec();
+
+         Europe eu = p.getEurope();
+
+         for (GoodsType good : s.getGoodsTypeList()) {
+         	List<AbstractGoods> goods = new ArrayList<AbstractGoods>();
+         	assertEquals(p.getMarket().getSalePrice(good, 1), eu.getOwner().getMarket().getSalePrice(good, 1));
+         	goods.add(new AbstractGoods(good, 1));
+         	int salePrice = p.getMarket().getSalePrice(good, 1);
+         	int bidPrice = p.getMarket().getBidPrice(good, 1);
+         	int buyCost = p.getMarket().getCostToBuy(good);
+         	int priceGoods = eu.priceGoods(goods);
+         	assertEquals(buyCost, bidPrice);
+         	assertEquals(buyCost, priceGoods);
+         	
+         	System.out.print("BuyCost: $");
+         	System.out.print(buyCost);
+         	System.out.print(" bidPrice: $");
+         	System.out.print(bidPrice);
+         	System.out.print(" salePrice: $");
+         	System.out.print(salePrice);
+         	System.out.print(" EU-priceGoods $");
+         	System.out.println(priceGoods);
+         }
+    }
+    
+    
 
     /**
      * Serialization and deserialization?
